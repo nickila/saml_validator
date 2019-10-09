7017d3d9a78c3fea2164f7bbf64dd3b560f4375b
michaelmernin <michael.mernin@gmail.com>
2019-10-09 14:20:26 -0500

    moved idp_info to yaml config file

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-from flask import Flask, render_template, request, jsonify
 import xmltodict
+from flask import Flask, render_template, request, jsonify
+import yaml
 from saml_validation import parse_saml, analyze
-from idp import IDP
 
 app = Flask(__name__, instance_relative_config=True)
 
 # from werkzeug.debug import DebuggedApplication
 # appx = DebuggedApplication(app, evalex=True)
+with open('configuration.yml') as file:
+    idp_repo = yaml.safe_load(file)
+
 
 @app.route('/')
 def welcome():
@@ -18,10 +21,10 @@ def upload():
     idp_name = request.values['idp_name']
     saml_dict = xmltodict.parse(request.files['saml_file'].read().decode())
     saml_values = parse_saml(saml_dict)
-    idp_info = IDP.get_idp_info(idp_name) if idp_name != 'other' else None
+    idp_info = idp_repo.get(idp_name, None)
     result = analyze(saml_values, idp_info)
 
-    return jsonify(result.result)
+    return jsonify(result)
 
 
 if __name__ == '__main__':
--- saml_validation.py
+++ saml_validation.py
@@ -21,23 +21,23 @@ def parse_saml(dict_saml_trace):
         saml_trace_values['assertion_attributes'] = assertion_attributes
     try:
         saml_trace_values['name_id'] = \
-        dict_saml_trace[key + 'p:Response'][key + ':Assertion'][key + ':Subject'][key + ':NameID']['#text']
+            dict_saml_trace[key + 'p:Response'][key + ':Assertion'][key + ':Subject'][key + ':NameID']['#text']
     except:
         saml_trace_values['name_id'] = None
     try:
         saml_trace_values['name_id_format'] = \
-        dict_saml_trace[key + 'p:Response'][key + ':Assertion'][key + ':Subject'][key + ':NameID']['@Format']
+            dict_saml_trace[key + 'p:Response'][key + ':Assertion'][key + ':Subject'][key + ':NameID']['@Format']
     except:
         saml_trace_values['name_id_format'] = None
     saml_trace_values['destination'] = dict_saml_trace[key + 'p:Response']['@Destination']
     saml_trace_values['issuer_url'] = \
-    dict_saml_trace[key + 'p:Response'][key + ':Assertion'][key + ':Conditions'][key + ':AudienceRestriction'][
-        key + ':Audience']
+        dict_saml_trace[key + 'p:Response'][key + ':Assertion'][key + ':Conditions'][key + ':AudienceRestriction'][
+            key + ':Audience']
     saml_trace_values['signature_method_algorithm'] = \
-    dict_saml_trace[key + 'p:Response']['ds:Signature']['ds:SignedInfo']['ds:SignatureMethod']['@Algorithm']
+        dict_saml_trace[key + 'p:Response']['ds:Signature']['ds:SignedInfo']['ds:SignatureMethod']['@Algorithm']
     saml_trace_values['digest_method_algorithm'] = \
-    dict_saml_trace[key + 'p:Response']['ds:Signature']['ds:SignedInfo']['ds:Reference']['ds:DigestMethod'][
-        '@Algorithm']
+        dict_saml_trace[key + 'p:Response']['ds:Signature']['ds:SignedInfo']['ds:Reference']['ds:DigestMethod'][
+            '@Algorithm']
     saml_trace_values['not_before'] = dict_saml_trace[key + 'p:Response'][key + ':Assertion'][key + ':Conditions'][
         '@NotBefore']
     saml_trace_values['time_sent'] = dict_saml_trace[key + 'p:Response']['@IssueInstant']
@@ -45,8 +45,8 @@ def parse_saml(dict_saml_trace):
         '@NotOnOrAfter']
     try:
         saml_trace_values['signing_cert'] = \
-        dict_saml_trace[key + 'p:Response']['ds:Signature']['ds:KeyInfo']['ds:X509Data'][
-            'ds:X509Certificate'] if True else None
+            dict_saml_trace[key + 'p:Response']['ds:Signature']['ds:KeyInfo']['ds:X509Data'][
+                'ds:X509Certificate'] if True else None
     except:
         saml_trace_values['signing_cert'] = None
     return saml_trace_values
@@ -56,23 +56,28 @@ def analyze(saml_values, idp_info=None):
     errors = {}
     if not saml_values['assertion_attributes']:
         errors['no_attributes'] = idp_info['error_codes']['assertion_attributes'] if idp_info \
-            else 'Assertion attributes not released'
+            else {'description': 'Assertion attributes not released'}
         errors['no_attributes']['description'] = 'Assertion attributes not released'
+
     if not {'FirstName', 'LastName', 'Email'}.issubset(saml_values['assertion_attributes']):
         errors['assertion_attributes'] = idp_info['error_codes']['assertion_attributes'] if idp_info \
-            else 'Assertion must include FirstName, LastName, and Email'
+            else {'description': 'Assertion must include FirstName, LastName, and Email'}
         errors['assertion_attributes']['description'] = 'Assertion must include FirstName, LastName, and Email'
+
     if not saml_values['name_id']:
         errors['name_id'] = idp_info['error_codes']['name_id'] if idp_info \
-            else 'Name-ID attribute must be present with unspecified or emailAddress format'
+            else {'description': 'Name-ID attribute must be present with unspecified or emailAddress format'}
         errors['name_id']['description'] = 'Name-ID attribute must be present with unspecified or emailAddress format'
+
     if saml_values['name_id_format'] not in ['unspecified', 'emailAddress']:
         errors['name_id_format'] = idp_info['error_codes']['name_id_format'] if idp_info \
-            else 'Name-ID requires unspecified or emailAddress format'
+            else {'description': 'Name-ID requires unspecified or emailAddress format'}
         errors['name_id_format']['description'] = 'Name-ID requires unspecified or emailAddress format'
+
     if not saml_values['signing_cert']:
         errors['signing_cert'] = idp_info['error_codes']['signing_cert'] if idp_info \
-            else 'Signing cert not present'
+            else {'description': 'Signing cert not present'}
         errors['signing_cert']['description'] = 'Signing cert not present'
+
     descriptions = Descriptions.get_descriptions()
-    return SamlResult(saml_values, descriptions, errors)
+    return SamlResult.construct_result(saml_values, descriptions, errors)
