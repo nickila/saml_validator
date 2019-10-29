82becf6b5f6c3b049b81b2185e46e11c72fe3c47
michaelmernin <michael.mernin@gmail.com>
2019-10-29 16:10:55 -0500

    refactor analyzer.create_error_dict()

--- backend_app/analyzer.py
+++ backend_app/analyzer.py
@@ -4,9 +4,9 @@ from backend_app.error_handler import SamlParsingError
 
 class Analyzer:
     @classmethod
-    def process(cls, saml_dict, descriptions, idp_info=None):
+    def process(cls, saml_dict, descriptions, idp_info):
         saml_values = cls.parse_saml(saml_dict)
-        errors = cls.create_error_dict(saml_values, descriptions.get('error_message'), idp_info.get('error_codes'))
+        errors = cls.create_error_dict(saml_values, descriptions.get('error_message'), idp_info.get('error_codes', {}))
         return ResponseBuilder.construct_response(saml_values, descriptions.get('metadata'), errors)
 
     @classmethod
@@ -79,7 +79,7 @@ class Analyzer:
         return value
 
     @classmethod
-    def create_error_dict(cls, saml_values, descriptions, idp_info=None):
+    def create_error_dict(cls, saml_values, descriptions, idp_info):
         """
         description: Compares saml values with the common errors below
         :type saml_values: dict(parsed saml values from xml upload)
@@ -90,29 +90,29 @@ class Analyzer:
         errors = {}
         if not saml_values['assertion_attributes']:
             errors['assertion_attributes'] = {'description': descriptions['no_attributes']}
-            if idp_info and idp_info.get('assertion_attributes'):
+            if idp_info.get('assertion_attributes'):
                 errors['assertion_attributes'].update(idp_info['assertion_attributes'])
         else:
             if not {'FirstName', 'LastName', 'Email'}.issubset(saml_values['assertion_attributes']):
                 errors['assertion_attributes'] = {
                     'description': descriptions['assertion_attributes']}
-                if idp_info and idp_info.get('assertion_attributes'):
+                if idp_info.get('assertion_attributes'):
                     errors['assertion_attributes'].update(idp_info['assertion_attributes'])
 
         if not saml_values['name_id']:
             errors['name_id'] = {'descriptions': descriptions['name_id']}
-            if idp_info and idp_info.get('name_id'):
+            if idp_info.get('name_id'):
                 errors['name_id'].update(idp_info['name_id'])
 
         name_id_format = saml_values['name_id_format']
         if 'unspecified' not in name_id_format and 'emailAddress' not in name_id_format:
             errors['name_id_format'] = {'description': descriptions['name_id_format']}
-            if idp_info and idp_info.get('name_id_format'):
+            if idp_info.get('name_id_format'):
                 errors['name_id_format'].update(idp_info['name_id_format'])
 
         if not saml_values['signing_cert']:
             errors['signing_cert'] = {'description': descriptions['signing_cert']}
-            if idp_info and idp_info.get('signing_cert'):
+            if idp_info.get('signing_cert'):
                 errors['signing_cert'].update(idp_info['signing_cert'])
 
         if not saml_values['in_response_to']:
